0381a7853770c4e73b97feb8a4d562dcef6b1218
jarleven <jarleven@gmail.com>
2021-11-22 20:20:05 +0100

    Update setup.sh
    Got it working manually, but not sure if I everything is in this script.

--- setup.sh
+++ setup.sh
@@ -2,13 +2,16 @@
 
 
 
-sudo apt remove -y python2
-sudo apt remove -y python-is-python2
-sudo apt autoremove -y --purge
+
 
 
 sudo apt update
 sudp apt upgrade -y
+
+sudo apt remove -y python2
+sudo apt remove -y python-is-python2
+sudo apt autoremove -y --purge
+sudo apt update
 
 sudo apt install -y git
 sudo apt install -y vim
@@ -21,16 +24,36 @@ sudo apt install -y libapache2-mod-wsgi-py3
 sudo apt-get install -y python-is-python3
 
 sudo pip3 install flask
+sudo pip3 install RPi.GPIO
 
 
-sudo cp -r flaskapp/ /var/www/html
 
-# cd ~
-# 
-# sudo ln -sT ~/flaskapp /var/www/html/flaskapp
-#
-# sudo -u www-data ls -lah /var/www/html/flaskapp
 
-# You need to edit the file below manually
-sudo vi /etc/apache2/sites-enabled/000-default.conf
+cd /var/www/html
+sudo wget https://raw.githubusercontent.com/naztronaut/RaspberryPi-browser-led-control/master/index.html
+sudo wget https://raw.githubusercontent.com/naztronaut/RaspberryPi-browser-led-control/master/script.js
+sudo wget https://raw.githubusercontent.com/naztronaut/RaspberryPi-browser-led-control/master/style.css
+
+
+sudo mkdir /var/www/led
+cd /var/www/led
+
+#sudo chown -R pi:pi .
+
+sudo wget https://raw.githubusercontent.com/naztronaut/RaspberryPi-RGBW-Control/master/utils/activate_this.py
+
+sudo wget https://raw.githubusercontent.com/naztronaut/RaspberryPi-browser-led-control/master/led/led.py
+sudo wget https://raw.githubusercontent.com/naztronaut/RaspberryPi-browser-led-control/master/led/led.wsgi
+
+
+
+# Modify led.wsgi
+# activate_this = '/var/www/led/activate_this.py'
+
+
+
+
+sudo a2ensite led.conf
+sudo a2dissite 000-default.conf
+sudo service apache2 restart
 
