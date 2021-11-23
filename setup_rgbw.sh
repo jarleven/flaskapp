c12174207c65eb2a40c42aeddd4523737031eaf8
jarleven <jarleven@gmail.com>
2021-11-23 22:08:44 +0100

    Update setup_rgbw.sh
    The diff, most of this is probably not needed

--- setup_rgbw.sh
+++ setup_rgbw.sh
@@ -102,3 +102,65 @@ sudo service apache2 restart
 # tail -f /var/log/apache2/other_vhosts_access.log
 # tail -f /var/log/apache2/error.log
 
+# In the crontab add
+# crontab -e
+# 
+# @reboot sudo pigpiod
+
+# My changes
+pi@raspberrypi:~ $ diff ~/RaspberryPi-RGBW-Control/utils/apache-led.conf  /etc/apache2/sites-available/apache-led.conf
+4c4
+<         WSGIScriptAlias /api/lr /var/www/html/rgbw/rgbw.wsgi
+---
+>         WSGIScriptAlias /html/rgbw /var/www/html/rgbw/rgbw.wsgi
+12c12
+< </VirtualHost>
+\ No newline at end of file
+---
+> </VirtualHost>
+
+pi@raspberrypi:~ $ diff ~/RaspberryPi-RGBW-Control/script.js  /var/www/html/script.js
+2c2
+<     url: 'http://192.168.1.225',
+---
+>     url: 'http://192.168.3.111',
+122c122
+<             url: `${config.url}/api/lr/?${queryBuilder}&${cacheBuster}`,
+---
+>             url: `${config.url}/html/rgbw/?${queryBuilder}&${cacheBuster}`,
+135c135
+<             url: `${config.url}/api/lr/white?white=${frequency}&${cacheBuster}`,
+---
+>             url: `${config.url}/html/rgbw/white?white=${frequency}&${cacheBuster}`,
+171c171
+<             url: `${config.url}/api/lr/getStatus?colors=${color}&${cacheBuster}`,
+---
+>             url: `${config.url}/html/rgbw/getStatus?colors=${color}&${cacheBuster}`,
+187c187
+< });
+\ No newline at end of file
+---
+> });
+
+
+
+pi@raspberrypi:~ $ diff ~/RaspberryPi-RGBW-Control/rgbw.py /var/www/html/rgbw/rgbw.py
+21c21
+<     # pi.set_PWM_dutycycle(18, white)
+---
+>     pi.set_PWM_dutycycle(18, white)
+
+
+pi@raspberrypi:~ $ diff ~/RaspberryPi-RGBW-Control/rgbw.wsgi /var/www/html/rgbw/rgbw.wsgi
+3c3
+< activate_this = '/var/www/html/rgbw/venv/bin/activate_this.py'
+---
+> activate_this = '/var/www/html/rgbw/activate_this.py'
+10c10
+< from rgbw import app as application
+\ No newline at end of file
+---
+> from rgbw import app as application
+
+
+
