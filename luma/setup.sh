173d406cdb32506befa2165722ddbd4d4632e5cb
jarleven <jarleven@gmail.com>
2021-12-13 21:40:01 +0100

    Update setup.sh

--- luma/setup.sh
+++ luma/setup.sh
@@ -6,7 +6,7 @@ sudo apt update
 sudo apt upgrade -y
 sudo apt install -y vim git
 
-  sudo usermod -a -G spi,gpio pi
+#sudo usermod -a -G spi,gpio pi
 sudo usermod -a -G spi,gpio,i2c pi
 
 sudo apt install -y python3 python3-pip python3-dev 
@@ -16,9 +16,6 @@ sudo apt install -y build-essential
 sudo apt install -y libjpeg-dev zlib1g-dev libfreetype6-dev liblcms2-dev libopenjp2-7 libtiff5
 
 
-
-
- 
 
 
 # The default pip and setuptools bundled with apt on Raspbian are really old, and can cause components to not be installed properly. Make sure they are up to date by upgrading them first:
