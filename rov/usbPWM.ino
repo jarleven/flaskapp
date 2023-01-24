90b0875b50a7f60905ef568dca3aa71a81451f84
jarleven <jarleven@gmail.com>
2023-01-24 15:03:14 +0100

    Update usbPWM.ino
    Tested on hardware

--- rov/usbPWM.ino
+++ rov/usbPWM.ino
@@ -1,9 +1,11 @@
 
+#define MA_PWM 5
+#define MA_A 3
+#define MA_B 4
 
-//https://forum.arduino.cc/t/read-line-from-serial/98251/5
-//https://stackoverflow.com/questions/11068450/arduino-c-language-parsing-string-with-delimiter-input-through-serial-interfa
-//https://www.electronicwings.com/users/sanketmallawat91/projects/215/frequency-changing-of-pwm-pins-of-arduino-uno#:~:text=The%20frequency%20of%20the%20PWM%20signal%20on%20most%20pins%20is,also%20run%20at%20980%20Hz.
-
+#define MB_PWM 6
+#define MB_A 7
+#define MB_B 8
 
 char buffer [32];
 int cnt = 0;
@@ -22,43 +24,71 @@ void runMotor(int motorA, int motorB)
 {
   Serial.println(motorA);
   Serial.println(motorB);
-
+  
+  
+ 
   if (motorA > 0){
-     digitalWrite(3, HIGH);
-     digitalWrite(4, LOW);
+    Serial.println("Forward A");
+     digitalWrite(MA_A, HIGH);
+     digitalWrite(MA_B, LOW);
   }
   else{
-     digitalWrite(3, LOW);
-     digitalWrite(4, HIGH);
+    Serial.println("Reverse A");
+    digitalWrite(MA_A, LOW);
+    digitalWrite(MA_B, HIGH);
   }
-
-  analogWrite(5,abs(motorA));
+  
+  if(motorA==0) {
+    Serial.println("STOP A");
+    digitalWrite(MA_A, LOW);
+    digitalWrite(MA_B, LOW);
+  }
+    analogWrite(MA_PWM,max(abs(motorA), 1023));
 
   if (motorB > 0){
-     digitalWrite(7, HIGH);
-     digitalWrite(8, LOW);
+    Serial.println("Forward B");
+     digitalWrite(MB_A, HIGH);
+     digitalWrite(MB_B, LOW);
   }
   else{
-     digitalWrite(7, LOW);
-     digitalWrite(8, HIGH);
+    Serial.println("Reverse B");
+     digitalWrite(MB_A, LOW);
+     digitalWrite(MB_B, HIGH);
   }
 
-  analogWrite(6,abs(motorB));
+  analogWrite(MB_PWM,max(abs(motorB),1023));
+  if(motorB==0) {
+    Serial.println("STOP B");
+    digitalWrite(MB_A, LOW);
+    digitalWrite(MB_B, LOW);
+  }
 
 }
 
 
 void printError(void) {
-  Serial.print("Oooops  : [");
+  Serial.print("Oooops  : ");
   Serial.print(buffer);
-  Serial.println("]");
+  runMotor(0, 0);
+
   errorMsg=false;
 }
 
 void setup() {
   // put your setup code here, to run once:
   pinMode(LED_BUILTIN, OUTPUT);
+     
+  pinMode(MA_A, OUTPUT);
+  pinMode(MA_B, OUTPUT);
+  pinMode(MA_PWM, OUTPUT); // PWM
+  pinMode(MB_PWM, OUTPUT); // PWM
+  pinMode(MB_A, OUTPUT);
+  pinMode(MB_B, OUTPUT);
+
+
   Serial.begin(115200);
+  Serial.println("EidVgs serial motor controller v0.1");
+  digitalWrite(LED_BUILTIN, HIGH);
 
 }
 
@@ -84,19 +114,30 @@ void loop() {
 
       if(sscanf(buffer, "%d,%d,%c", &motorA, &motorB, &endOfLine) == 3){
 
-        if(endOfLine="s")
+        if(endOfLine=='s')
           runMotor(motorA, motorB);
         else
           errorMsg=true;
       }
       else
         errorMsg=true;
+
+
+
     
       // Cleanup and get ready for new input string
       ready=false;  
     }
 
-  if(errorMsg)
+  if(errorMsg) {
     printError();
-    
+    //Sticky error message
+    digitalWrite(LED_BUILTIN, LOW);
+  }
+        
 }
+
+
+//https://forum.arduino.cc/t/read-line-from-serial/98251/5
+//https://stackoverflow.com/questions/11068450/arduino-c-language-parsing-string-with-delimiter-input-through-serial-interfa
+//https://www.electronicwings.com/users/sanketmallawat91/projects/215/frequency-changing-of-pwm-pins-of-arduino-uno#:~:text=The%20frequency%20of%20the%20PWM%20signal%20on%20most%20pins%20is,also%20run%20at%20980%20Hz.
