671bd3dfd78b321233f799d74af8846ac27006e7
jarleven <jarleven@gmail.com>
2023-02-18 15:25:15 +0100

    Use serialport

--- rov/rov.py
+++ rov/rov.py
@@ -1,7 +1,11 @@
 from flask import Flask, request, jsonify
 import json
 import logging
- 
+import serial 
+
+# Arduino Uno is normally /dev/ttyACM0. Setup non blocking serialport
+ser = serial.Serial('/dev/ttyACM0', 115200, timeout=0,  write_timeout=0)
+
 
 app = Flask(__name__)
 
@@ -9,21 +13,6 @@ app = Flask(__name__)
 logging.basicConfig(filename='record.log', level=logging.DEBUG, format=f'%(asctime)s %(levelname)s %(name)s %(threadName)s : %(message)s')
 
 app.logger.info('Is this ever printed')
-
-def pwm(val):
-    
-    if(abs(val) < 0.1):
-    	val=0
-
-    if(val > 0):
-        fwd = val*255
-        rev = 0
-    else:
-        fwd = 0
-        rev = abs(val)*255
-
-    return fwd, rev
-
 
 
 # {{url}}/led?status=on
@@ -39,9 +28,15 @@ def led():
     # Axis 3 right
     right = float(request.args.get('green'))
 
-    app.logger.info('RGB LED function %f  %f' % (left, right))
+    #app.logger.info('RGB LED function %f  %f' % (left, right))
     
     # send Left and Right values over the serial port
+    test=("%s,%s,s\n" % (str(int(right * 255)), str(int(left * 255))))
+    #app.logger.info(test)
+    
+    if serial.out_waiting() < 20:
+        ser.write(test.encode())
+
 
     return jsonify({"red": left, "green": right, "blue": 0})
 
