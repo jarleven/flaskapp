d04ee1346d50b62b798438816ad0ad907aa5670f
jarleven <jarleven@gmail.com>
2023-02-20 21:14:26 +0100

    Simple UDP server

---
+++ rov/Simple_UDP_Server/gameCnt.py
@@ -0,0 +1,118 @@
+#!/usr/bin/env python3
+
+# https://raw.githubusercontent.com/Footleg/pygame-controller/master/examples/SentinelBoard/SimpleTwinMotor.py
+# https://stackoverflow.com/questions/18743962/python-send-udp-packet
+
+
+
+""" Example of a robot using tank steering with two motors for left and right tracks.
+    This robot uses the Sentinel board from Footleg Robotics. The speed and steering are
+    controlled from the left and right sticks respectively, so in this example the
+    change handler functions store the stick postions in variables and these are
+    used to update the motor powers in the main program loop. (See the PiconZero
+    Simple Twin Motor example for an alternative way to control a robot with just a
+    single stick for both power and steering).
+"""
+import pygame, random, time, math
+from pygamecontroller import RobotController
+#import sentinelboard
+
+import socket
+
+UDP_IP = "127.0.0.1"
+UDP_IP = "192.168.3.178"
+
+UDP_PORT = 5005
+MESSAGE = "Hello, World!"
+
+print("UDP target IP:", UDP_IP)
+print("UDP target port:", UDP_PORT)
+print("message:", MESSAGE)
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # UDP
+
+
+
+
+
+
+#Initialise global variables
+
+right = 0
+left = 0
+power = 0
+turn = 0
+minMovingSpeed = 30
+message = ""
+
+#Multiplier applied to speed so top speed is set when stick is not quite at maximum position
+speedFactor = 1.0
+
+#sb = sentinelboard.SentinelBoard()
+
+
+
+def initStatus(status):
+    """Callback function which displays status during initialisation"""
+    if status == 0 :
+        print("Supported controller connected")
+    elif status < 0 :
+        print("No supported controller detected")
+    else:
+        print("Waiting for controller {}".format( status ) )
+
+
+def leftStickChangeHandler(valLR, valUD):
+    """Handler function for left analogue stick"""
+    global left
+    left = valUD
+
+
+def rightStickChangeHandler(valLR, valUD):
+    """Handler function for right analogue stick"""
+    global right
+    right = valUD
+
+
+def main():
+    global message
+    ## Check that required hardware is connected ##
+
+    #Initialise the controller board
+
+
+    #Run in try..finally structure so that program exits gracefully on hitting any
+    #errors in the callback functions
+    try:
+        cnt = RobotController("Turbo 4WD Robot", initStatus,
+                              leftStickChanged = leftStickChangeHandler,
+                              rightStickChanged = rightStickChangeHandler)
+
+        if cnt.initialised :
+            keepRunning = True
+            #Indicate success here, we are ready to run
+            print (' yippee!!!!!')
+        else:
+            keepRunning = False
+
+        # -------- Main Program Loop -----------
+        while keepRunning == True :
+            cnt.message = message
+
+
+            # Trigger stick events and check for quit
+            keepRunning = cnt.controllerStatus()
+    
+            message=("%s,%s,s\n" % (str(int(right * 255)), str(int(left * 255))))
+            #message = "40,30,s".format(power,turn)
+            print(message)
+            sock.sendto(bytes(message, "utf-8"), (UDP_IP, UDP_PORT))
+            #motorSpeed(power, turn)
+    finally:
+        #Clean up and turn off Blinkt LEDs
+        #sb.allOff()
+        pygame.quit()
+
+
+if __name__ == '__main__':
+    main()
---
+++ rov/Simple_UDP_Server/udpServer.py
@@ -0,0 +1,56 @@
+# https://wiki.python.org/moin/UdpCommunication
+
+import socket
+
+import serial 
+import serial.tools.list_ports
+import time
+
+
+UDP_IP = "192.168.3.178"
+UDP_PORT = 5005
+
+myports = [tuple(p) for p in list(serial.tools.list_ports.comports())]
+print("List of serialports :")
+print(myports)
+
+# Arduino Uno is normally /dev/ttyACM0.
+serport="not-found"
+    
+if 'ttyACM0' in str(myports):
+    serport="/dev/ttyACM0"
+    print("Use : %s" % serport)
+elif 'ttyUSB0' in str(myports):
+    serport="/dev/ttyUSB0"
+    print("Use : %s" % serport)
+
+if serport == "not-found":
+    print("Did not find any Arduino, sayonnara")
+    exit()
+
+ser = serial.Serial(serport, 115200, timeout=0,  writeTimeout=0)
+print("Wait for serial port to reset....")
+time.sleep(3)
+
+
+sock = socket.socket(socket.AF_INET,    # Internet
+                     socket.SOCK_DGRAM) # UDP
+sock.bind((UDP_IP, UDP_PORT))
+
+# Keep track of serialport activity
+busy=0
+write=0
+
+while True:
+    data, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
+    print("received message: %s" % data)
+
+    #data=("%s,%s,s\n" % (str(int(right * 255)), str(int(left * 255))))
+    if ser.out_waiting < 20:
+        print("Busy is %d written %d" % (busy, write))
+        print(data)
+        ser.write(data)
+        write=write+1
+            
+    else:
+        busy=busy+1
